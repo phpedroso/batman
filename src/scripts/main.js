ee29c3c233ab3f83b8cda63d7748cfca47cef09f
PHP <php3droso@gmail.com>
2023-05-17 15:22:42 -0300

    First commit

---
+++ src/scripts/main.js
@@ -0,0 +1,24 @@
+AOS.init();
+
+const dataDoEvento = new Date("Nov 18, 2023 20:00:00");
+const timeStampdoEvento = dataDoEvento.getTime();
+
+const contaTempo = setInterval(function () {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteEvento = timeStampdoEvento - timeStampAtual;
+
+    const diasAteEvento = Math.floor(distanciaAteEvento / (1000 * 60 * 60 * 24));
+    const horasAteEvento = Math.floor((distanciaAteEvento % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutosAteEvento = Math.floor(distanciaAteEvento % (1000 * 60 * 60) / (1000 * 60));
+    const segundosAteEvento =  Math.floor(distanciaAteEvento % (1000 * 60 ) / 1000);
+
+    document.getElementById('contator').innerHTML = `faltam ${diasAteEvento}d ${horasAteEvento}h ${minutosAteEvento}min ${segundosAteEvento}s`;
+
+    if (distanciaAteEvento < 0 ) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contator').innerHTML = '';
+    }
+}, 1000);
+
